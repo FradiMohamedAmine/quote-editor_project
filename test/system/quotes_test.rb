27ee54e618c5b5dbb243e3aa8e9b56a1ee613d81
FradiMohamedAmine <aminfradi614@gmail.com>
2024-07-02 13:14:02 +0100

    turbo frames and turbo streams

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -5,20 +5,23 @@ class QuotesTest < ApplicationSystemTestCase
     @quote = quotes(:first) # Reference to the first fixture quote
   end
 
+  test "Showing a quote" do
+    visit quotes_path
+    click_link @quote.name
+
+    assert_selector "h1", text: @quote.name
+  end
+
   test "Creating a new quote" do
     # When we visit the Quotes#index page
     # we expect to see a title with the text "Quotes"
     visit quotes_path
     assert_selector "h1", text: "Quotes"
 
-    # When we click on the link with the text "New quote"
-    # we expect to land on a page with the title "New quote"
     click_on "New quote"
-    assert_selector "h1", text: "New quote"
-
-    # When we fill in the name input with "Capybara quote"
-    # and we click on "Create Quote"
     fill_in "Name", with: "Capybara quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Create quote"
 
     # We expect to be back on the page with the title "Quotes"
@@ -26,6 +29,7 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
     assert_text "Capybara quote"
   end
+
   test "Showing a quote" do
     visit quotes_path
     click_link @quote.name
@@ -38,9 +42,9 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
-
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
@@ -54,5 +58,4 @@ class QuotesTest < ApplicationSystemTestCase
     click_on "Delete", match: :first
     assert_no_text @quote.name
   end
-
 end
