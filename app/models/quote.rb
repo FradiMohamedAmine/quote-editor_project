f35fcbea6fad4ca3da4ae13e7e23bbca8de981f9
FradiMohamedAmine <aminfradi614@gmail.com>
2024-07-04 09:54:02 +0100

    CRUD on dates without using Turbo Frames and Turbo Streams

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,6 @@
 class Quote < ApplicationRecord
-    belongs_to :company
+  has_many :line_item_dates, dependent: :destroy
+  belongs_to :company
 
 
     validates :name, presence: true
@@ -8,7 +9,7 @@ class Quote < ApplicationRecord
 
   # after_create_commit -> { broadcast_prepend_later_to "quotes" }
   # after_update_commit -> { broadcast_replace_later_to "quotes" }
-  #after_destroy_commit ->(quote) { broadcast_remove_to [ quote.company, "quotes" ] }
+  # after_destroy_commit ->(quote) { broadcast_remove_to [ quote.company, "quotes" ] }
   # Those three callbacks are equivalent to the following single line
   broadcasts_to ->(quote) { [ quote.company, "quotes" ] }, inserts_by: :prepend
 end
